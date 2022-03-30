651acaa061a3bd3d6d9c3e073684d21da3d98136
LouisPetatLenoir <louis.petat.lenoir@orange.fr>
2022-03-30 15:47:34 +0200

    ajout calendrier

---
+++ www/login/accueil.php
@@ -0,0 +1,23 @@
+<?php
+class Date{
+
+    var $days = array('Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche');
+    var $months = array('Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Décembre');
+
+    function getAll($year){
+        $r= array();
+
+        $date = new Datetime($year.'-01-01');
+        while($date->format('Y')<=$year){
+            $y = $date->format('Y');
+            $m = $date->format('m');
+            $d = $date->format('j');
+            $w = str_replace('0','7',$date->format('w'));
+            $r[$y][$m][$d] = $w;
+            $date->add(new DateInterval('P1D'));
+
+        }
+        return $r;
+    }
+
+}
--- www/login/success.php
+++ www/login/success.php
@@ -12,6 +12,28 @@ if (!isset($_SESSION["username"])) {
 
 <head>
     <link rel="stylesheet" href="../assets/css/style.css" />
+    <script src="http://code.jquery.com/jquery.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        jQuery(function($){
+            $('.month').hide();
+            $('.month:first').show();
+            $('.months a:first').addClass('active');
+            var current = 1;
+            $('.months a').click(function(){
+                var month =$(this).attr('id').replace('linkMonth','');
+                if (month != current)(
+                    $('#month'+current).slideUp();
+                    $('#month'+month).slideDown();
+                    $('.months a').removeClass('active');
+                    $('.months a#linkMonth'+month).addClass('active');
+                    current= month;
+                )
+                alert(month);
+                return false;
+            });
+
+        });
+    </script>
 </head>
 
 <body>
@@ -20,6 +42,65 @@ if (!isset($_SESSION["username"])) {
         <p>C'est votre tableau de bord.</p>
         <a href="logout.php">Déconnexion</a>
     </div>
+
+    <?php
+    require('accueil.php');
+    $date = new Date();
+    $year = date('Y');
+    $month = date('m');
+    $dates = $date->getAll($year);
+    ?>
+    <div class="periods">
+        <div class="year"><?php echo $year; ?></div>
+
+        <div class="months">
+            <ul>
+                <?php foreach ($date->months as $id=>$m): ?>
+                    <li><a href='#' id="linkMonth<?php echo $id+1; ?>"><?php echo utf8_encode(substr(utf8_decode($m),0,3)); ?></li>
+                <?php endforeach; ?>
+            </ul>
+    </div>
+    <div class="clear"></div>
+
+    <?php $dates = current($dates); ?>
+    <?php foreach ($dates as $m=>$days): ?>
+        <div class="month" id="month<?php echo $m; ?>">
+        <table>
+            <thead>
+                <tr>
+                    <?php foreach ($date->days as $d): ?>
+                        <th> <?php echo substr($d,0,3); ?></th>
+                    <?php endforeach; ?>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <?php $end= end($days); foreach ($days as $d=>$w): ?>
+                    <?php if($d == 1): ?>
+                        <td colspan="<?php echo $w-1; ?>"> </td>
+                    <?php endif; ?>
+                    <td><?php echo $d; ?></td>
+
+                    <?php if($w == 7): ?>
+                        </tr><tr>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+
+               <?php if ($end != 7): ?>
+                    <td colspan="<?php echo 7-$end; ?>"></td>
+                <?php endif; ?>
+
+               </tr>
+            </tbody>
+        </table>
+        </div>
+    <?php endforeach; ?>
+
+
+
+
+
+    <pre> <?php print_r($dates); ?> </pre>
 </body>
 
 </html>
