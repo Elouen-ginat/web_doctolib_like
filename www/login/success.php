591f16916289071b64fdcb312bff5df1ebbd429b
elouen_ginat <elouen.ginat@eleve.ensai.fr>
2022-04-01 19:37:22 +0200

    page de selection des docteurs

--- www/login/success.php
+++ www/login/success.php
@@ -4,8 +4,10 @@ session_start();
 // Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
 if (!isset($_SESSION["username"])) {
     header("Location: login.php");
-    exit();}
+    exit();
+}
 
+echo "doctor_id = ". $_SESSION["doctor_id"];
 ?>
 
 <!DOCTYPE html>
@@ -39,41 +41,41 @@ if (!isset($_SESSION["username"])) {
 </head>
 
 <body>
-<?php
-$response = file_get_contents('http://localhost/api/doctor.php/lastname');
-$response = json_decode($response);
-foreach ($response as $id){
-    $nom =$id->lastname;
-    $prenom =$id->firstname;
-    $office =$id->office;
-    $num =$id->phone;
-    $infos = 'Docteur '.$nom.' '.$prenom."<br> Cabinet :".$office."<br> Numéro :".$num."<br>";
-}
+    <?php
+    $response = file_get_contents('http://localhost/api/doctor.php/lastname');
+    $response = json_decode($response);
+    foreach ($response as $id) {
+        $nom = $id->lastname;
+        $prenom = $id->firstname;
+        $office = $id->office;
+        $num = $id->phone;
+        $infos = 'Docteur ' . $nom . ' ' . $prenom . "<br> Cabinet :" . $office . "<br> Numéro :" . $num . "<br>";
+    }
 
-?>
+    ?>
 
     <div class="sucess">
         <h1>Bienvenue <?php echo $_SESSION['username']; ?>!</h1>
-        <p>C'est votre tableau de bord.  </p>
+        <p>C'est votre tableau de bord. </p>
         <a href="logout.php">Déconnexion</a>
     </div>
 
     <div class="medecin">
-            <thead>
-                <tr>
-                    <?php foreach ($response as $id): ?>
-                        <th> <?php echo $infos; ?></th>
-                    <?php endforeach; ?>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
+        <thead>
+            <tr>
+                <?php foreach ($response as $id) : ?>
+                    <th> <?php echo $infos; ?></th>
+                <?php endforeach; ?>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
 
-               </tr>
-            </tbody>
-        </div>
+            </tr>
+        </tbody>
+    </div>
 
-<!--
+    <!--
 
 
     require('accueil.php');
@@ -87,39 +89,40 @@ foreach ($response as $id){
 
         <div class="months">
             <ul>
-                <?php foreach ($date->months as $id=>$m): ?>
-                    <li><a href='#' id="linkMonth<?php echo $id+1; ?>"><?php echo utf8_encode(substr(utf8_decode($m),0,3)); ?></li>
+                <?php foreach ($date->months as $id => $m) : ?>
+                    <li><a href='#' id="linkMonth<?php echo $id + 1; ?>"><?php echo utf8_encode(substr(utf8_decode($m), 0, 3)); ?></li>
                 <?php endforeach; ?>
             </ul>
     </div>
     <div class="clear"></div>
 
     <?php $dates = current($dates); ?>
-    <?php foreach ($dates as $m=>$days): ?>
+    <?php foreach ($dates as $m => $days) : ?>
         <div class="month" id="month<?php echo $m; ?>">
         <table>
             <thead>
                 <tr>
-                    <?php foreach ($date->days as $d): ?>
-                        <th> <?php echo substr($d,0,3); ?></th>
+                    <?php foreach ($date->days as $d) : ?>
+                        <th> <?php echo substr($d, 0, 3); ?></th>
                     <?php endforeach; ?>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                <?php $end= end($days); foreach ($days as $d=>$w): ?>
-                    <?php if($d == 1): ?>
-                        <td colspan="<?php echo $w-1; ?>"> </td>
+                <?php $end = end($days);
+                foreach ($days as $d => $w) : ?>
+                    <?php if ($d == 1) : ?>
+                        <td colspan="<?php echo $w - 1; ?>"> </td>
                     <?php endif; ?>
                     <td><?php echo $d; ?></td>
 
-                    <?php if($w == 7): ?>
+                    <?php if ($w == 7) : ?>
                         </tr><tr>
                     <?php endif; ?>
                 <?php endforeach; ?>
 
-               <?php if ($end != 7): ?>
-                    <td colspan="<?php echo 7-$end; ?>"></td>
+               <?php if ($end != 7) : ?>
+                    <td colspan="<?php echo 7 - $end; ?>"></td>
                 <?php endif; ?>
 
                </tr>
