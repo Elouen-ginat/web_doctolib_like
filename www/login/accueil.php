6cb50f6a3aedd10799e7acbb7212f5509ead6c27
LouisPetatLenoir <louis.petat.lenoir@orange.fr>
2022-04-01 23:11:12 +0200

    calendrier qui marche mais à mettre au propre avec style

--- www/login/accueil.php
+++ www/login/accueil.php
@@ -13,14 +13,88 @@ class Date{
             $y = $date->format('Y');
             $m = $date->format('m');
             $d = $date->format('j');
-            $h = $date->format('H');
-            $i = $date->format('i');
             $w = str_replace('0','7',$date->format('w'));
-            $r[$y][$m][$d][$h][$i] = $w;
+            $r[$y][$m][$d] = $w;
             $date->add(new DateInterval('P1D'));
 
         }
         return $r;
     }
+    function next_day($day_number)
+    {
+      for ($i = 2; $i <= 8; $i++)
+      {
+        $next_day = mktime(0,0,0, date("m"), date("d")+$i, date("Y"));
+        if(date("w",$next_day)==$day_number)
+        {
+          $XDate = getdate($next_day);
+          $next_day_fund = sprintf('%02d', $XDate['mday']).'-'.sprintf('%02d', $XDate['mon']).'-'.sprintf('%02d', $XDate['year']);
+        }
+      }
+      return $next_day_fund;
+    }
+
+    function get_day($day){
+        // date du jour
+        $date = date($day);
+        // tableau des jours de la semaine
+        $joursem = array('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday');
+        // extraction des jour, mois, an de la date
+        list($jour, $mois, $annee) = explode('/', $date);
+        // calcul du timestamp
+        $timestamp = mktime (0, 0, 0, $mois, $jour, $annee);
+        // affichage du jour de la semaine
+        return $joursem[date("w",$timestamp)];
+
+    }
+
+    function timestamp_date($d,$m,$y,$h,$mn,$s ){
+        return mktime($h,$mn, $s, $m,  $d, $y);
+    }
+
+    function getMinPlage($min){
+        return $min*60;
+    }
+    function calandarbydate($date, $start , $end ){
+
+        $dateTab = explode('-',$date);
+        $startTab = explode(':',$start);
+        $endTab = explode(':',$end);
+        $b = timestamp_date($dateTab[0],$dateTab[1],$dateTab[2],$startTab[0],$startTab[1],$startTab[2]);
+
+        $a = array('date'=>$date,
+          'start'=>$b,
+          'end'=> timestamp_date( $dateTab[0],$dateTab[1],$dateTab[2],$endTab[0],$endTab[1],$endTab[2]),
+          'register' =>  array());
+
+
+        return $a;}
+
+    function viewCalandar($calandar ,$plage ){
+            foreach ($calandar as $key => $values){
+
+            $nbdispoplage =   round(( $values['end'] - $values['start'] )/ $plage)  ;
+            $timestampDate  = $values['start'] +$nbdispoplage;
+            echo '<br/><u>'.$nbdispoplage. ' plages de '.($plage/60).'mn disponible pour le '.$values['date'].'</u><br/>';
+            for( $i=0;$i< $nbdispoplage ;$i++){
+                $style= "";
+                $timestampDate  = $values['start'] + ( $i*$plage );
+                if($timestampDate + $plage > $values['end'] ){
+                $style='style="color:red"';
+                }
+                    foreach ($values['register'] as $k => $v ){
+                        if($timestampDate >= $v['start']  && $timestampDate < $v['end'] ){
+                            $style='style="color:red"';
+                        }
+                        else if($timestampDate + $plage >  $v['start']  && $timestampDate < $v['end'] ){
+                            $style='style="color:red"';
+                        }
+                    }
+                echo"<span $style >".date("H:i:s", $timestampDate).'</span> | ';
+            }
+
+        }
+    }
 
 }
+?>
--- www/login/success.php
+++ www/login/success.php
@@ -14,28 +14,6 @@ if (!isset($_SESSION["username"])) {
 <head>
     <link rel="stylesheet" href="../assets/css/style.css" />
     <script src="http://code.jquery.com/jquery.js" type="text/javascript"></script>
-    <!--<script type="text/javascript">
-        jQuery(function($){
-            $('.month').hide();
-            /*$('.month:first').show();*/
-            $('.months a:first').addClass('active');
-            var current = 1;
-            $('.months a').click(function(){
-                var month = $(this).attr('id').replace('linkMonth','');
-                if (month != current){
-                    $('#month'+current).slideUp();
-                    $('#month'+month).slideDown();
-                    $('.months a').removeClass('active');
-                    $('.months a#linkMonth'+month).addClass('active');
-                    current = month;
-                }
-                return false;
-            });
-
-        });
-    </script>-->
-
-
 </head>
 
 <body>
@@ -47,92 +25,83 @@ foreach ($response as $id){
     $prenom =$id->firstname;
     $office =$id->office;
     $num =$id->phone;
+    $start_hour= $id->str_hour;
+    $end_hour=$id->end_hour;
     $infos = 'Docteur '.$nom.' '.$prenom."<br> Cabinet :".$office."<br> Numéro :".$num."<br>";
 }
 
+function timestamp_date($d,$m,$y,$h,$mn,$s ){
+        return mktime($h,$mn, $s, $m,  $d, $y);
+    }
+
+
+function calandarbydate($date, $start , $end ){
+
+        $dateTab = explode('-',$date);
+        $startTab = explode(':',$start);
+        $endTab = explode(':',$end);
+        $b = timestamp_date($dateTab[0],$dateTab[1],$dateTab[2],$startTab[0],$startTab[1],$startTab[2]);
+
+        $a = array('date'=>$date,
+          'start'=>$b,
+          'end'=> timestamp_date( $dateTab[0],$dateTab[1],$dateTab[2],$endTab[0],$endTab[1],$endTab[2]),
+          'register' =>  array());
+
+
+        return $a;}
+
+
+
+require('accueil.php');
+$date = new Date();
+$year = date('Y');
+$today_m =floatval(date('m'));
+if($today_m <10){
+    $today_m = substr(str_repeat(0, 1).$today_m, 0);
+                }
+$today_d = floatval(date('j'));
+$dates = $date->getAll($year);
+$min = 15;
+$plage= $date->getMinPlage($min);
 ?>
+
 
     <div class="sucess">
         <h1>Bienvenue <?php echo $_SESSION['username']; ?>!</h1>
         <p>C'est votre tableau de bord.  </p>
         <a href="logout.php">Déconnexion</a>
+
     </div>
 
     <div class="medecin">
             <thead>
                 <tr>
                     <?php foreach ($response as $id): ?>
-                        <th> <?php echo $infos; ?></th>
-                    <?php endforeach; ?>
+                        <th> <?php $nom =$id->lastname;
+                        $prenom =$id->firstname;
+                        $office =$id->office;
+                        $num =$id->phone;
+                        $start_hour= $id->str_hour;
+                        $end_hour=$id->end_hour;
+                        $infos = '<br> Docteur '.$nom.' '.$prenom."<br> Cabinet :".$office."<br> Numéro :".$num."<br>";
+                        echo $infos;
+                        $calandar = array();
+                        foreach(range(0, 6) as $number){
+                            $j = $date->next_day($number);
+                            $calandar[$j] = calandarbydate($j, $start_hour ,  $end_hour );
+
+                            $calandar[$j]['register'][] =  calandarbydate($j, "8:30:0" ,  "9:30:0" );
+                            }
+                        echo $date->viewCalandar($calandar ,$plage );
+
+                        ?></th>
+                    <?php endforeach;
+                    ?>
                 </tr>
             </thead>
-            <tbody>
-                <tr>
 
-               </tr>
-            </tbody>
-        </div>
-
-<!--
-
-
-    require('accueil.php');
-    $date = new Date();
-    $year = date('Y');
- /*   $events = $date->getEvents($year);*/
-    $dates = $date->getAll($year);
-    ?>
-    <div class="periods">
-        <div class="year"><?php echo $year; ?></div>
-
-        <div class="months">
-            <ul>
-                <?php foreach ($date->months as $id=>$m): ?>
-                    <li><a href='#' id="linkMonth<?php echo $id+1; ?>"><?php echo utf8_encode(substr(utf8_decode($m),0,3)); ?></li>
-                <?php endforeach; ?>
-            </ul>
     </div>
-    <div class="clear"></div>
 
-    <?php $dates = current($dates); ?>
-    <?php foreach ($dates as $m=>$days): ?>
-        <div class="month" id="month<?php echo $m; ?>">
-        <table>
-            <thead>
-                <tr>
-                    <?php foreach ($date->days as $d): ?>
-                        <th> <?php echo substr($d,0,3); ?></th>
-                    <?php endforeach; ?>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <?php $end= end($days); foreach ($days as $d=>$w): ?>
-                    <?php if($d == 1): ?>
-                        <td colspan="<?php echo $w-1; ?>"> </td>
-                    <?php endif; ?>
-                    <td><?php echo $d; ?></td>
-
-                    <?php if($w == 7): ?>
-                        </tr><tr>
-                    <?php endif; ?>
-                <?php endforeach; ?>
-
-               <?php if ($end != 7): ?>
-                    <td colspan="<?php echo 7-$end; ?>"></td>
-                <?php endif; ?>
-
-               </tr>
-            </tbody>
-        </table>
-        </div>
-    <?php endforeach; ?>
-
-
-
-
-
-    <pre> <?php print_r($dates); ?> </pre>-->
 </body>
 
 </html>
